5d4ad5063df6721ffa0dc8eb2db73a1928454baa
sboris001 <sboris001@gmail.com>
2024-02-22 23:05:30 -0700

    updated(library) - the poster adding system kind of works now

--- startup-html/library.js
+++ startup-html/library.js
@@ -11,6 +11,14 @@ function movieName() {
     window.location.href = "library.html";
 }
 
+function getMovieName() {
+    return localStorage.getItem("movie");
+}
+
+function getRating() {
+    return localStorage.getItem("rating");
+}
+
 function rating() {
     const nameEl = document.querySelector("#rating")
     localStorage.setItem("rating", nameEl.value);
@@ -32,4 +40,60 @@ function createPoster(src, alt, height, title, rating) {
     divElement.appendChild(paragraph);
 
     return divElement
+}
+
+function getMovieInfoPromise(title) {
+    const cleanTitle = title.split(" ");
+    let string = "";
+    for (const element of cleanTitle) {
+      if (element !== cleanTitle[cleanTitle.length - 1]) {
+        string = string + element + "+";
+      } else {
+        string = string + element;
+      }
+    }
+    const query = `http://www.omdbapi.com/?t=${string}&apikey=4fcd8264`
+
+    return fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            return {
+                title: data.Title,
+                poster: data.Poster
+            };
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            throw error;
+        });
+}
+
+
+function addMovie() {
+    const movieTitle = document.getElementById('movie').value;
+    const rating = document.getElementById('rating').value;
+    fetchAndDisplayMoviePoster(movieTitle, rating);
+    return false;
+}
+
+function fetchAndDisplayMoviePoster(movieTitle, rating) {
+    getMovieInfoPromise(movieTitle)
+        .then(movieData => {
+            const { title, poster } = movieData;
+            const targetSection = document.querySelector('.librarySection');
+
+            const imageData = {
+                src: poster,
+                alt: title,
+                height: '250',
+                title: title,
+                rating: rating
+            };
+
+            const image = createPoster(imageData.src, imageData.alt, imageData.height, imageData.title, imageData.rating);
+            targetSection.appendChild(image);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
