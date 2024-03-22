c8813d21ed010f8a48286b16ea406fe04f7586c4
sboris001 <sboris001@gmail.com>
2024-03-21 19:21:01 -0600

    Added some imports and set up my router

--- startup-html/index.js
+++ startup-html/index.js
@@ -1,22 +1,29 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
-// The service port. In production the frontend code is statically hosted by the service on the same port.
+// The service port may be set on the command line
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
-// Serve up the frontend static content hosting
+// Serve up the applications static content
 app.use(express.static('public'));
 
-// Router for service endpoints
-const apiRouter = express.Router();
-app.use(`/api`, apiRouter);
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 // Login endpoint
 apiRouter.post('/login', (req, res) => {
