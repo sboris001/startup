6cb0e36990d3f7206c783128d8058478259f2b7a
sboris001 <sboris001@gmail.com>
2024-04-12 14:30:38 -0600

    changed root to work with the changes I've made

--- startup-html/service/index.js
+++ startup-html/service/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serve up the applications static content
-app.use(express.static('public'));
+app.use(express.static('../startup-html'));
 
 // Trust headers that are forwarded from the proxy so we can determine IP addresses
 app.set('trust proxy', true);
@@ -125,7 +125,7 @@ apiRouter.get('/getAllMovies', async (req, res) => {
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
+    res.sendFile('index.html', { root: '../startup-html' });
 });
   
 const httpService = app.listen(port, () => {
