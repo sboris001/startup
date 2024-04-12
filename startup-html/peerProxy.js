378f0c435e1f197821ab326ae2bbf13f751de285
sboris001 <sboris001@gmail.com>
2024-04-12 12:56:32 -0600

    Added to my peerProxy

--- startup-html/peerProxy.js
+++ startup-html/peerProxy.js
@@ -1,32 +1,3 @@
-// const { WebSocketServer, OPEN } = require('ws'); // Import WebSocketServer and OPEN from 'ws'
-
-// function peerProxy(httpServer) {
-//     const wss = new WebSocketServer({ server: httpServer });
-
-//     // In your peerProxy.js file, modify the WebSocket message handling to process movie ratings
-//     wss.on('connection', function(ws) {
-//         console.log('Client connected');
-
-//         ws.on('message', function(message) {
-//             // Broadcast the received message to all connected clients
-//             wss.clients.forEach(function(client) {
-//                 if (client !== ws && client.readyState === OPEN) {
-//                     client.send(message);
-//                 }
-//             });
-
-//             const ratingData = JSON.parse(message);
-//             console.log('Received movie rating:', ratingData);
-//         });
-
-//         ws.on('close', function() {
-//             console.log('Client disconnected');
-//         });
-//     });
-// }
-
-// module.exports = { peerProxy };
-
 const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
 
