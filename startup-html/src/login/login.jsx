b96bc932a8780899c0346e01f5af4469b591e11f
sboris001 <sboris001@gmail.com>
2024-04-12 21:41:34 -0600

    Got the Login page looking right -- No functionality yet

--- startup-html/src/login/login.jsx
+++ startup-html/src/login/login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
-  return (
-    <main>
-      <div>Login displayed here</div>
-    </main>
-  );
+    const loginUser = () => {
+        // Implement login functionality
+    };
+
+    const createUser = () => {
+        // Implement create user functionality
+    };
+
+    return (
+        <main>
+            <div className="cameraIcon"><img src="assets/cameraIcon.svg" className="icon" height="105" width="105" alt="Camera Icon" /></div>
+            <div className="center">
+                <form className="login" method="get">
+                    <h3>Movie Rater</h3>
+                    <input type="text" id="name" placeholder="Username" />
+                    <input type="password" id="password" placeholder="Password" />
+                    <button className="btn1" onClick={(event) => { event.preventDefault(); loginUser(); }}>Login</button>
+                    <button className="btn1" onClick={(event) => { event.preventDefault(); createUser(); }}>Create</button>
+                </form>
+            </div>
+        </main>
+    );
 }
