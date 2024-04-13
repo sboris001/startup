61aef4fc96d90ce6ae04b62992364c015f56c849
sboris001 <sboris001@gmail.com>
2024-04-12 20:09:20 -0600

    I now display a template page using react

--- startup-html/src/app.jsx
+++ startup-html/src/app.jsx
@@ -2,5 +2,38 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+    return (
+        <html lang="en" className="bg">
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="index.css" />
+                <link rel="stylesheet" href="styles.css" />
+                <title>Home</title>
+            </head>
+            <body>
+                <header>
+                    <nav>
+                        <menu className="topnav">
+                            <a href="index.html">Home</a>
+                            <a href="library.html">Library</a>
+                            <a href="leaderboard.html">Leaderboard</a>
+                        </menu>
+                    </nav>
+                </header>
+
+                <main>
+                    <div className="center">
+                    </div>
+                </main>
+
+                <footer>
+                    <section className="author">
+                        <p>&nbsp;&nbsp;&nbsp;Spencer Boris: &nbsp;&nbsp;&nbsp;<a href="https://github.com/sboris001/startup">GitHub</a></p>
+                    </section>
+                </footer>
+
+            </body>
+        </html>
+    );
+  }
