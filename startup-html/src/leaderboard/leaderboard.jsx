c38ebb2f4b5a201275803d9e29c110971098208c
sboris001 <sboris001@gmail.com>
2024-04-12 23:08:33 -0600

    I have the html and css looking the way I want - time for javascript

--- startup-html/src/leaderboard/leaderboard.jsx
+++ startup-html/src/leaderboard/leaderboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import "./leaderboard.css";
 
 export function Leaderboard() {
-  return (
-    <main>
-      <div>Leaderboard displayed here</div>
-    </main>
-  );
+    return (
+        <main style={{ paddingBottom: '25px' }}>
+            <section className="spacing"></section>
+            <div className="leaderBoardTitle">
+                Movie Ratings Leaderboard
+            </div>
+
+            <table id="leaderboard-table">
+                {/* Table rows will be dynamically populated by JavaScript */}
+            </table>
+        </main>
+    );
 }
--- startup-html/src/library/library.jsx
+++ startup-html/src/library/library.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import './library.css';
 
 export function Library() {
-  return (
-    <main>
-      <div>Library displayed here</div>
-    </main>
-  );
+    return (
+        <main>
+            <section className="mainTop">
+                <section className="addMovie">
+                    <div className="addPlacement">
+                        <p>Add Movie</p>
+                        <div style={{ padding: "0 0 0.5rem 0" }}>
+                            <form className="addForm">
+                                <div className="gridItem">Movie:&nbsp;&nbsp;</div>
+                                <div className="formItem">&nbsp;&nbsp;<input className="input" type="text" name="movie" id="movie" placeholder="Enter a movie title" /></div>
+                                <div className="gridItem">Your rating:&nbsp;&nbsp;</div>
+                                <div className="formItem">&nbsp;&nbsp;<input className="input" type="text" name="rating" id="rating" placeholder="Enter your rating" /></div>
+                                <div className="btn"><button className="btn2" id="add-button" type="submit">Add</button></div>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+                <div className="right">
+                    <section className="alertSection">
+                        <div className="alert event">
+                            <div id="player-messages">
+                                <div className="event">See other users' ratings here!</div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </section>
+
+            <section className="titleSection">
+                <div className="title"><h1 id="userName"></h1><h1>'s Library</h1></div>
+                {/* Assuming displayUserName is implemented elsewhere */}
+                {/* <script>
+                    displayUserName();
+                </script> */}
+            </section>
+            <section className="librarySection">
+                {/* Here you can populate the librarySection with content */}
+            </section>
+        </main>
+    );
 }
