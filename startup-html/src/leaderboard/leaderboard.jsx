17f01d1667271d53bdd84d3d7ffcec4be8f58e76
sboris001 <sboris001@gmail.com>
2024-04-12 20:25:35 -0600

    Created very basic placeholders for each route

---
+++ startup-html/src/leaderboard/leaderboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Login() {
+  return (
+    <main>
+      <div>Leaderboard displayed here</div>
+    </main>
+  );
+}
---
+++ startup-html/src/library/library.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Library() {
+  return (
+    <main>
+      <div>Library displayed here</div>
+    </main>
+  );
+}
